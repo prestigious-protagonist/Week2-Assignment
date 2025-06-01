94c8a0e123e6752c3e48bec1da1787e8411a92e8
prestigious-protagonist <jaskaranyt123@gmail.com>
2025-06-01 11:05:30 +0530

    updated statusCodes

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
